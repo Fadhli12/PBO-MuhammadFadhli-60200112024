b693e2ea3439674356c97eb078c3f984cb79e2bc
Muhammad Fadhli <thegenesispath@gmail.com>
2015-06-28 16:05:20 +0800

    Praktikum PBO
    60200112024 Muhammad Fadhli E

---
+++ src/praktikum3/concat.java
@@ -0,0 +1,12 @@
+package praktikum3;
+
+public class concat {
+
+	public static void main(String[] args) {
+		String str1 = "Nama :";
+		String str2 = " Muhammad Fadhli";
+		String str3 = str1 + str2;
+		System.out.println(str3);
+	}
+
+}
